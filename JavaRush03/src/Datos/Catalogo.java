178f6faf82a5bc2ce5e3d4ef95149ac49e20ef03
Alvaro-C <alvarocanasboal@gmail.com>
2021-08-05 17:21:06 +0200

    añadido metodo para listar juegos, cambiado Catalogo de List a ArrayList

--- JavaRush03/src/Datos/Catalogo.java
+++ JavaRush03/src/Datos/Catalogo.java
@@ -1,36 +1,67 @@
 package Datos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import modelo.Juego;
 
 public class Catalogo {
 
-	List<Juego> catalogo;
+	ArrayList<Juego> catalogo;
 
+	
+
+	
 	public Catalogo() {
 		super();
 	}
 
-	public Catalogo(List<Juego> catalogo) {
+
+	
+
+	public Catalogo(ArrayList<Juego> catalogo) {
 		super();
 		this.catalogo = catalogo;
 	}
 
-	public List<Juego> getCatalogo() {
+
+
+
+	public ArrayList<Juego> getCatalogo() {
 		return catalogo;
 	}
 
-	public void setCatalogo(List<Juego> catalogo) {
+
+
+	public void setCatalogo(ArrayList<Juego> catalogo) {
 		this.catalogo = catalogo;
 	}
+
+	
+
 
 	@Override
 	public String toString() {
 		return "Catalogo [catalogo=" + catalogo + "]";
 	}
-	
-	
+
+
+
+
+	public void imprimirCatalogo() {
+		
+		Juego[] j = new Juego[0];
+		
+		Juego[] arrayJuegos = this.catalogo.toArray(j);
+		
+		for (int i = 0; i < arrayJuegos.length; i++) {
+			
+			System.out.println(arrayJuegos[i].toString());
+			
+		}
+		
+		
+	}
 	
 	
 }
