1b1d86775996f6e2ca63b7a15ea6d12cc2b5a910
unknown <joseantonioramos24@gmail.com>
2021-08-05 16:29:01 +0200

    Forzar error

--- JavaRush03/src/control/Main.java
+++ JavaRush03/src/control/Main.java
@@ -3,7 +3,11 @@ package control;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+		System.out.println("Forzar error");
 
 	}
 
