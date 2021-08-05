f102b589e17e62aa6d7552290f4b157a83a7fd3e
Alvaro-C <alvarocanasboal@gmail.com>
2021-08-05 18:10:40 +0200

    pequeño switch para hacer pruebas e imprimir juegos

--- JavaRush03/src/control/Main.java
+++ JavaRush03/src/control/Main.java
@@ -1,11 +1,40 @@
 package control;
 
+import Datos.Catalogo;
+import gui.menus;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Forzar error");
+		opcionesMenuPrincipal();
+		
 
 	}
+	
+	public static void opcionesMenuPrincipal() {
+		
+		Catalogo c = new Catalogo();
+		int option = menus.menuInicial();
+		
+		switch(option) {
+		
+		case 1: 
+			System.out.println("Estamos trabajando en ello");
+			break;
+		case 2: 
+			
+			System.out.println("Listado de todos los juegos");
+			c.imprimirCatalogo();
+			
+			break;
+		
+		
+		}
+		
+		
+		
+	}
+	
 
 }
