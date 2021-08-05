86fe984909a607d44daf4abcb84c5d7ded140d89
Alvaro-C <alvarocanasboal@gmail.com>
2021-08-05 16:31:34 +0200

    añadido paquete modelo, clase juego, conflicto en Main

--- JavaRush03/src/control/Main.java
+++ JavaRush03/src/control/Main.java
@@ -1,9 +1,13 @@
 package control;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
 
 	}
 
---
+++ JavaRush03/src/modelo/Juego.java
@@ -0,0 +1,80 @@
+package modelo;
+
+public class Juego {
+
+	int rank;
+	String nombre;
+	String plataforma;
+	int ano;
+	String genero;
+	String editor;
+
+	public Juego() {
+		super();
+	}
+
+	public Juego(int rank, String nombre, String plataforma, int ano, String genero, String editor) {
+		super();
+		this.rank = rank;
+		this.nombre = nombre;
+		this.plataforma = plataforma;
+		this.ano = ano;
+		this.genero = genero;
+		this.editor = editor;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getEditor() {
+		return editor;
+	}
+
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+
+	@Override
+	public String toString() {
+		return "Juego [rank=" + rank + ", nombre=" + nombre + ", plataforma=" + plataforma + ", ano=" + ano
+				+ ", genero=" + genero + ", editor=" + editor + "]";
+	}
+
+}
