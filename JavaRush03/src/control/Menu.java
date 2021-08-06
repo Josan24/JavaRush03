fb07e5fca83892f8c1ad91938cfa7b1a1ef633cd
geostvn <geo.stvn@gmail.com>
2021-08-06 09:51:36 +0200

    Cambio estructura y capa control

---
+++ JavaRush03/src/control/Menu.java
@@ -0,0 +1,62 @@
+package control;
+
+import Datos.Catalogo;
+import gui.PantallaMenus;
+import services.GestionCatalogo;
+
+public class Menu {
+	public static void controlMenu(Catalogo c) {
+		int option = 0;
+		
+		do {
+			option = PantallaMenus.menuInicial();
+			
+			switch(option) {
+			
+			case 1:
+				GestionCatalogo gc = new GestionCatalogo();			
+				System.out.println("Añadir un nuevo juego");
+				gc.agregarAlCatalogo(c);
+				
+				break;
+			case 2:
+				
+				System.out.println("Listado de juegos Genero:\"plataforma\"");
+				
+				break;
+			case 3:
+				System.out.println("Listado de juegos");
+				c.imprimirCatalogo();
+				
+				break;
+			case 4:
+				System.out.println("Juegos para consolas de Nintendo");
+				break;
+			case 5:
+				System.out.println("Listado de editores");
+				break;
+			case 6:
+				System.out.println("Los juegos del siglo XX");
+				break;
+			case 7:
+				System.out.println("Gestión de juegos (Edit, Delete)");
+				//Buscar juego
+				//llamar menuEditarjuego(juego);
+				
+				break;
+			case 8:
+				System.out.println("Listados de juegos (filtrados por todo tipo de géneros)");
+				break;
+			case 9:
+				System.out.println("Los juegos que aparecen en años pares");
+				break;
+			case 0:
+				System.out.println("Salir");
+				break;
+			}
+			
+		}while(option != 0);
+		
+	}
+
+}
--- JavaRush03/src/gui/PantallaMenus.java
+++ JavaRush03/src/gui/PantallaMenus.java
@@ -3,23 +3,23 @@ package gui;
 import modelo.Juego;
 import utilidades.LecturaDatos;
 
-public class Menus {
+public class PantallaMenus {
 	
 	public static int menuInicial() {
 		int opcion = 0;
 
         System.out.print("**** Menú inicial ****\n" +
-            "1) Gestión de juegos (altas)\n" +
-            "2) Listado de juegos Género:\"plataforma\"\n" +
+            "1) Gestion de juegos (altas)\n" +
+            "2) Listado de juegos Genero:\"plataforma\"\n" +
             "3) Listado de juegos\n" +
             "4) Juegos para consolas de Nintendo\n" +
             "5) Listado de editores\n" +
             "6) Los juegos del siglo XX\n" +
-            "7) Gestión de juegos (Edit, Delete)\n" +
-            "8) Listados de juegos (filtrados por todo tipo de géneros)\n" +
-            "9) Los juegos que aparecen en años pares\n" +
+            "7) Gestion de juegos (Edit, Delete)\n" +
+            "8) Listados de juegos (filtrados por todo tipo de generos)\n" +
+            "9) Los juegos que aparecen en anos pares\n" +
             "0) Salir\n");
-        opcion = LecturaDatos.leerInteger("Seleciona una opción: ");
+        opcion = LecturaDatos.leerInteger("Seleciona una opcion: ");
            
         return opcion;
 	}
@@ -27,11 +27,11 @@ public class Menus {
 	public static int menuGestionJuegos() {
 		int opcion = 0;
 
-        System.out.print("**** Menú Gestión de Juegos ****\n" +
+        System.out.print("**** Menú Gestion de Juegos ****\n" +
             "1) Editar Juego\n" +
             "2) Eliminar Juego\n" +
-            "0) Atrás\n");
-        opcion = LecturaDatos.leerInteger("Seleciona una opción: ");
+            "0) Atras\n");
+        opcion = LecturaDatos.leerInteger("Seleciona una opcion: ");
            
         return opcion;
 	}
